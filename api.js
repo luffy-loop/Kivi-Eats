7b7e9c204aafc037d000c71cde320aa6f554cf9b
PoojaSri <poojasrikandhula@gmail.com>
2026-03-20 10:47:48 +0530

    menuData.js

--- api.js
+++ api.js
@@ -1,1 +1,61 @@
 
+const menuItems = [
+
+/* BREAKFAST */
+{ name: "Plain Dosa", price: 40, category: "Breakfast" },
+{ name: "Masala Dosa", price: 60, category: "Breakfast" },
+{ name: "Onion Dosa", price: 70, category: "Breakfast" },
+{ name: "Idli (2 pcs)", price: 30, category: "Breakfast" },
+{ name: "Vada", price: 25, category: "Breakfast" },
+{ name: "Upma", price: 35, category: "Breakfast" },
+{ name: "Pongal", price: 45, category: "Breakfast" },
+
+/* STARTERS */
+{ name: "Veg Manchurian", price: 120, category: "Starters" },
+{ name: "Paneer 65", price: 150, category: "Starters" },
+{ name: "Chicken 65", price: 180, category: "Starters" },
+{ name: "Chilli Chicken", price: 200, category: "Starters" },
+{ name: "French Fries", price: 90, category: "Starters" },
+
+/* LUNCH VEG */
+{ name: "Veg Meals", price: 120, category: "Lunch" },
+{ name: "Paneer Butter Masala", price: 150, category: "Lunch" },
+{ name: "Dal Tadka", price: 90, category: "Lunch" },
+{ name: "Veg Biryani", price: 130, category: "Lunch" },
+{ name: "Jeera Rice", price: 80, category: "Lunch" },
+
+/* LUNCH NON VEG */
+{ name: "Chicken Biryani", price: 200, category: "Lunch" },
+{ name: "Butter Chicken", price: 220, category: "Lunch" },
+{ name: "Egg Curry", price: 120, category: "Lunch" },
+
+/* ROTIS */
+{ name: "Chapati", price: 10, category: "Rotis" },
+{ name: "Butter Roti", price: 20, category: "Rotis" },
+{ name: "Aloo Paratha", price: 40, category: "Rotis" },
+{ name: "Paneer Paratha", price: 60, category: "Rotis" },
+
+/* DINNER */
+{ name: "Lemon Rice", price: 50, category: "Dinner" },
+{ name: "Curd Rice", price: 60, category: "Dinner" },
+{ name: "Tomato Rice", price: 55, category: "Dinner" },
+{ name: "Veg Khichdi", price: 80, category: "Dinner" },
+
+/* SANDWICH */
+{ name: "Veg Sandwich", price: 50, category: "Sandwich" },
+{ name: "Grilled Cheese", price: 70, category: "Sandwich" },
+{ name: "Chicken Sandwich", price: 90, category: "Sandwich" },
+
+/* DRINKS */
+{ name: "Masala Buttermilk", price: 20, category: "Drinks" },
+{ name: "Cold Coffee", price: 90, category: "Drinks" },
+{ name: "Lime Soda", price: 40, category: "Drinks" },
+{ name: "Mojito", price: 70, category: "Drinks" },
+
+/* DESSERTS */
+{ name: "Gulab Jamun", price: 40, category: "Desserts" },
+{ name: "Ice Cream", price: 60, category: "Desserts" },
+{ name: "Chocolate Brownie", price: 80, category: "Desserts" },
+{ name: "Rasgulla", price: 50, category: "Desserts" }
+
+];
